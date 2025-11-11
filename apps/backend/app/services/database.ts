44166f151224186e61eac9a7c9f3fbc60bd1ee17
Cameron Tredoux <camtredoux@gmail.com>
2025-11-10 20:28:18 -0800

    chore: kysely instance

---
+++ apps/backend/app/services/database.ts
@@ -0,0 +1,16 @@
+import { DB } from "#prisma/generated/types";
+import env from "#start/env";
+import { Kysely, PostgresDialect } from "kysely";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: env.get("DATABASE_URL"),
+});
+
+const dialect = new PostgresDialect({
+  pool,
+});
+
+export const db = new Kysely<DB>({
+  dialect,
+});
