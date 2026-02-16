a91bd02386d64ea0ca27e00c4adf3e4aa01c627d
Cameron Tredoux <camtredoux@gmail.com>
2026-02-16 13:44:15 -0800

    feat: hover card shows underline for link

--- apps/frontend/src/features/explore/components/schools/school-card/school-card.tsx
+++ apps/frontend/src/features/explore/components/schools/school-card/school-card.tsx
@@ -10,7 +10,7 @@ interface SchoolCardProps {
 
 export function SchoolCard({ school, isFollowing }: SchoolCardProps) {
   return (
-    <div className="from-brand/10 via-background to-background hover:from-brand/16 hover:via-brand/8 relative flex flex-row gap-3 overflow-clip rounded-2xl border bg-linear-to-br bg-clip-padding p-3 transition-colors duration-75 contain-[layout_style_paint] sm:items-center sm:p-4">
+    <div className="group from-brand/10 via-background to-background hover:from-brand/16 hover:via-brand/8 relative flex flex-row gap-3 overflow-clip rounded-2xl border bg-linear-to-br bg-clip-padding p-3 transition-colors duration-75 contain-[layout_style_paint] sm:items-center sm:p-4">
       <CardBackground />
 
       <div className="flex h-full min-w-0 flex-1 items-start gap-2 sm:flex-row sm:gap-3">
