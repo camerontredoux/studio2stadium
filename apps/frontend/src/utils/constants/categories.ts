73b118136b1dd9aae993a91c97f7d22e8f2d77f9
Cameron Tredoux <camtredoux@gmail.com>
2026-02-16 16:29:08 -0800

    fix: wrong key name

--- apps/frontend/src/utils/constants/categories.ts
+++ apps/frontend/src/utils/constants/categories.ts
@@ -1,6 +1,6 @@
 export const VIDEO_CATEGORIES = {
   choreography: "Choreography",
-  "strengthening exercises": "Strengthening Exercises",
+  "strengthening-exercises": "Strengthening Exercises",
   technique: "Technique",
   propointers: "Propointers",
 } as const;
