9f7da32c64575bb4a4c8084208dd7b7912831ec8
Cameron Tredoux <camtredoux@gmail.com>
2026-02-18 00:55:51 -0800

    chore: debugging

--- apps/backend/start/cron.ts
+++ apps/backend/start/cron.ts
@@ -4,5 +4,6 @@ import CacheSkillsCommand from "../services/cache-skills.ts";
 const service = new CacheSkillsCommand();
 
 new CronJob("0 * * * *", async () => {
+  console.log("[Cron]: Staring cron job for skill rarity cache");
   await service.run();
 }).start();
