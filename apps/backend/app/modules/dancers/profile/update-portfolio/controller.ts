12ee6f40558ca9540dfe2042cdb3a64e004000dd
Cameron Tredoux <camtredoux@gmail.com>
2026-02-25 01:09:58 -0800

    chore: revert

--- apps/backend/app/modules/dancers/profile/update-portfolio/controller.ts
+++ apps/backend/app/modules/dancers/profile/update-portfolio/controller.ts
@@ -10,8 +10,6 @@ export default class UpdatePortfolioController {
 
     await service.execute(ctx.session.profileId, payload);
 
-    await ctx.auth.use("redis").bump();
-
     return ctx.response.noContent();
   }
 }
