41fe773bd7a79fa1b3dc04bf6a57e25bf5f3df9e
Cameron Tredoux <camtredoux@gmail.com>
2026-03-06 13:41:47 -0800

    fix: hardcoded url

--- apps/backend/app/modules/users/delete-account/goodbye-email.ts
+++ apps/backend/app/modules/users/delete-account/goodbye-email.ts
@@ -1,3 +1,4 @@
+import env from "#start/env";
 import { BaseMail } from "@adonisjs/mail";
 
 interface GoodbyeEmailData {
@@ -20,7 +21,7 @@ export default class GoodbyeEmail extends BaseMail {
       `<p>Hi ${firstName},</p>
 <p>Your Studio2Stadium account has been successfully deleted.</p>
 <p>All of your data has been removed from our systems.</p>
-<p>If you ever want to come back, we'd love to have you. You can create a new account anytime at studio2stadium.com.</p>
+<p>If you ever want to come back, we'd love to have you. You can create a new account anytime at <a href="${env.get("SITE_URL")}">${env.get("SITE_URL")}</a>.</p>
 <p>Thank you for being part of our community.</p>
 <p>Best wishes,<br>The Studio2Stadium Team</p>`
     );
@@ -31,7 +32,7 @@ Your Studio2Stadium account has been successfully deleted.
 
 All of your data has been removed from our systems.
 
-If you ever want to come back, we'd love to have you. You can create a new account anytime at studio2stadium.com.
+If you ever want to come back, we'd love to have you. You can create a new account anytime at ${env.get("SITE_URL")}.
 
 Thank you for being part of our community.
 
