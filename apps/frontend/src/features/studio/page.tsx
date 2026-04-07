d29c50bd31cab51b34febacce3513f9b60c0427b
liamsmith234 <lism5572@colorado.edu>
2026-04-07 04:17:28 -0600

    Update page.tsx

--- apps/frontend/src/features/studio/page.tsx
+++ apps/frontend/src/features/studio/page.tsx
@@ -1,7 +1,9 @@
+import References from "./components/References";
 import { About } from "./components/about";
 
 export function StudioPage() {
   return <div>
     <About description="Description goes here!"></About>
+    <References />
     </div>;
 }
