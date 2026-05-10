de4823c3ce428bc010ff4791724d2678850caa4e
Cameron Tredoux <camtredoux@gmail.com>
2026-05-09 23:26:30 -0700

    feat: add delete callback service and controller

---
+++ apps/backend/app/modules/orgs/scouting/callbacks/delete/controller.ts
@@ -0,0 +1,18 @@
+import { inject } from "@adonisjs/core";
+import type { HttpContext } from "@adonisjs/core/http";
+import { DeleteCallbackService } from "./service.ts";
+
+export default class DeleteCallbackController {
+  @inject()
+  async handle(ctx: HttpContext, service: DeleteCallbackService) {
+    if (!ctx.orgRoster) {
+      return ctx.response.conflict({
+        message: "You must be registered in this event as a coach to scout.",
+      });
+    }
+
+    const dancerRosterId = ctx.params.dancerRosterId as string;
+    await service.execute(ctx.orgEvent!.id, ctx.orgRoster.id, dancerRosterId);
+    return ctx.response.noContent();
+  }
+}
